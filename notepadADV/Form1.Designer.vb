4af106da74cdb473cf549ceb8eb4d404c83d1d9e
BedantaDeyCH <87112582+BedantaDeyCH@users.noreply.github.com>
2021-07-08 11:34:02 +0530

    Add files via upload

---
+++ notepadADV/Form1.Designer.vb
@@ -0,0 +1,311 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class NotepadADV
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.FileToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.NewFileToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.OpenFileToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.SaveFileToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ExitNotepadADVToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.EditToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.CutToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.CopyToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.PasteToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.UndoToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.RedoOperationToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.FormatToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.FontFormattingToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ColorFormattingToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.AlignTextToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.LeftToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.RightToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.CenterToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.InsertBulletsToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.YesToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.NoToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ZoomLevelToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ToolStripMenuItem2 = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ToolStripMenuItem3 = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ToolStripMenuItem4 = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ToolStripMenuItem5 = New System.Windows.Forms.ToolStripMenuItem()
+        Me.AboutToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.RichTextBox1 = New System.Windows.Forms.RichTextBox()
+        Me.SaveFileDialog1 = New System.Windows.Forms.SaveFileDialog()
+        Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
+        Me.FontDialog1 = New System.Windows.Forms.FontDialog()
+        Me.ColorDialog1 = New System.Windows.Forms.ColorDialog()
+        Me.HelpToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.MenuStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.FileToolStripMenuItem, Me.EditToolStripMenuItem, Me.FormatToolStripMenuItem, Me.AboutToolStripMenuItem, Me.HelpToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(843, 24)
+        Me.MenuStrip1.TabIndex = 0
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'FileToolStripMenuItem
+        '
+        Me.FileToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.NewFileToolStripMenuItem, Me.OpenFileToolStripMenuItem, Me.SaveFileToolStripMenuItem, Me.ExitNotepadADVToolStripMenuItem})
+        Me.FileToolStripMenuItem.Name = "FileToolStripMenuItem"
+        Me.FileToolStripMenuItem.Size = New System.Drawing.Size(37, 20)
+        Me.FileToolStripMenuItem.Text = "File"
+        '
+        'NewFileToolStripMenuItem
+        '
+        Me.NewFileToolStripMenuItem.Name = "NewFileToolStripMenuItem"
+        Me.NewFileToolStripMenuItem.Size = New System.Drawing.Size(165, 22)
+        Me.NewFileToolStripMenuItem.Text = "New file"
+        '
+        'OpenFileToolStripMenuItem
+        '
+        Me.OpenFileToolStripMenuItem.Name = "OpenFileToolStripMenuItem"
+        Me.OpenFileToolStripMenuItem.Size = New System.Drawing.Size(165, 22)
+        Me.OpenFileToolStripMenuItem.Text = "Open file"
+        '
+        'SaveFileToolStripMenuItem
+        '
+        Me.SaveFileToolStripMenuItem.Name = "SaveFileToolStripMenuItem"
+        Me.SaveFileToolStripMenuItem.Size = New System.Drawing.Size(165, 22)
+        Me.SaveFileToolStripMenuItem.Text = "Save file"
+        '
+        'ExitNotepadADVToolStripMenuItem
+        '
+        Me.ExitNotepadADVToolStripMenuItem.Name = "ExitNotepadADVToolStripMenuItem"
+        Me.ExitNotepadADVToolStripMenuItem.Size = New System.Drawing.Size(165, 22)
+        Me.ExitNotepadADVToolStripMenuItem.Text = "Exit NotepadADV"
+        '
+        'EditToolStripMenuItem
+        '
+        Me.EditToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.CutToolStripMenuItem, Me.CopyToolStripMenuItem, Me.PasteToolStripMenuItem, Me.UndoToolStripMenuItem, Me.RedoOperationToolStripMenuItem})
+        Me.EditToolStripMenuItem.Name = "EditToolStripMenuItem"
+        Me.EditToolStripMenuItem.Size = New System.Drawing.Size(39, 20)
+        Me.EditToolStripMenuItem.Text = "Edit"
+        '
+        'CutToolStripMenuItem
+        '
+        Me.CutToolStripMenuItem.Name = "CutToolStripMenuItem"
+        Me.CutToolStripMenuItem.Size = New System.Drawing.Size(159, 22)
+        Me.CutToolStripMenuItem.Text = "Cut"
+        '
+        'CopyToolStripMenuItem
+        '
+        Me.CopyToolStripMenuItem.Name = "CopyToolStripMenuItem"
+        Me.CopyToolStripMenuItem.Size = New System.Drawing.Size(159, 22)
+        Me.CopyToolStripMenuItem.Text = "Copy"
+        '
+        'PasteToolStripMenuItem
+        '
+        Me.PasteToolStripMenuItem.Name = "PasteToolStripMenuItem"
+        Me.PasteToolStripMenuItem.Size = New System.Drawing.Size(159, 22)
+        Me.PasteToolStripMenuItem.Text = "Paste"
+        '
+        'UndoToolStripMenuItem
+        '
+        Me.UndoToolStripMenuItem.Name = "UndoToolStripMenuItem"
+        Me.UndoToolStripMenuItem.Size = New System.Drawing.Size(159, 22)
+        Me.UndoToolStripMenuItem.Text = "Undo Operation"
+        '
+        'RedoOperationToolStripMenuItem
+        '
+        Me.RedoOperationToolStripMenuItem.Name = "RedoOperationToolStripMenuItem"
+        Me.RedoOperationToolStripMenuItem.Size = New System.Drawing.Size(159, 22)
+        Me.RedoOperationToolStripMenuItem.Text = "Redo Operation"
+        '
+        'FormatToolStripMenuItem
+        '
+        Me.FormatToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.FontFormattingToolStripMenuItem, Me.ColorFormattingToolStripMenuItem, Me.ZoomLevelToolStripMenuItem, Me.AlignTextToolStripMenuItem, Me.InsertBulletsToolStripMenuItem})
+        Me.FormatToolStripMenuItem.Name = "FormatToolStripMenuItem"
+        Me.FormatToolStripMenuItem.Size = New System.Drawing.Size(57, 20)
+        Me.FormatToolStripMenuItem.Text = "Format"
+        '
+        'FontFormattingToolStripMenuItem
+        '
+        Me.FontFormattingToolStripMenuItem.Name = "FontFormattingToolStripMenuItem"
+        Me.FontFormattingToolStripMenuItem.Size = New System.Drawing.Size(152, 22)
+        Me.FontFormattingToolStripMenuItem.Text = "Font Face"
+        '
+        'ColorFormattingToolStripMenuItem
+        '
+        Me.ColorFormattingToolStripMenuItem.Name = "ColorFormattingToolStripMenuItem"
+        Me.ColorFormattingToolStripMenuItem.Size = New System.Drawing.Size(152, 22)
+        Me.ColorFormattingToolStripMenuItem.Text = "Font Color"
+        '
+        'AlignTextToolStripMenuItem
+        '
+        Me.AlignTextToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.LeftToolStripMenuItem, Me.RightToolStripMenuItem, Me.CenterToolStripMenuItem})
+        Me.AlignTextToolStripMenuItem.Name = "AlignTextToolStripMenuItem"
+        Me.AlignTextToolStripMenuItem.Size = New System.Drawing.Size(152, 22)
+        Me.AlignTextToolStripMenuItem.Text = "Align Text"
+        '
+        'LeftToolStripMenuItem
+        '
+        Me.LeftToolStripMenuItem.Name = "LeftToolStripMenuItem"
+        Me.LeftToolStripMenuItem.Size = New System.Drawing.Size(152, 22)
+        Me.LeftToolStripMenuItem.Text = "Left"
+        '
+        'RightToolStripMenuItem
+        '
+        Me.RightToolStripMenuItem.Name = "RightToolStripMenuItem"
+        Me.RightToolStripMenuItem.Size = New System.Drawing.Size(152, 22)
+        Me.RightToolStripMenuItem.Text = "Right"
+        '
+        'CenterToolStripMenuItem
+        '
+        Me.CenterToolStripMenuItem.Name = "CenterToolStripMenuItem"
+        Me.CenterToolStripMenuItem.Size = New System.Drawing.Size(152, 22)
+        Me.CenterToolStripMenuItem.Text = "Center"
+        '
+        'InsertBulletsToolStripMenuItem
+        '
+        Me.InsertBulletsToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.YesToolStripMenuItem, Me.NoToolStripMenuItem})
+        Me.InsertBulletsToolStripMenuItem.Name = "InsertBulletsToolStripMenuItem"
+        Me.InsertBulletsToolStripMenuItem.Size = New System.Drawing.Size(152, 22)
+        Me.InsertBulletsToolStripMenuItem.Text = "Insert Bullets"
+        '
+        'YesToolStripMenuItem
+        '
+        Me.YesToolStripMenuItem.Name = "YesToolStripMenuItem"
+        Me.YesToolStripMenuItem.Size = New System.Drawing.Size(152, 22)
+        Me.YesToolStripMenuItem.Text = "Yes"
+        '
+        'NoToolStripMenuItem
+        '
+        Me.NoToolStripMenuItem.Name = "NoToolStripMenuItem"
+        Me.NoToolStripMenuItem.Size = New System.Drawing.Size(152, 22)
+        Me.NoToolStripMenuItem.Text = "No"
+        '
+        'ZoomLevelToolStripMenuItem
+        '
+        Me.ZoomLevelToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ToolStripMenuItem2, Me.ToolStripMenuItem3, Me.ToolStripMenuItem4, Me.ToolStripMenuItem5})
+        Me.ZoomLevelToolStripMenuItem.Name = "ZoomLevelToolStripMenuItem"
+        Me.ZoomLevelToolStripMenuItem.Size = New System.Drawing.Size(152, 22)
+        Me.ZoomLevelToolStripMenuItem.Text = "Font Size"
+        '
+        'ToolStripMenuItem2
+        '
+        Me.ToolStripMenuItem2.Name = "ToolStripMenuItem2"
+        Me.ToolStripMenuItem2.Size = New System.Drawing.Size(152, 22)
+        Me.ToolStripMenuItem2.Text = "+ 2%"
+        '
+        'ToolStripMenuItem3
+        '
+        Me.ToolStripMenuItem3.Name = "ToolStripMenuItem3"
+        Me.ToolStripMenuItem3.Size = New System.Drawing.Size(152, 22)
+        Me.ToolStripMenuItem3.Text = "+ 4%"
+        '
+        'ToolStripMenuItem4
+        '
+        Me.ToolStripMenuItem4.Name = "ToolStripMenuItem4"
+        Me.ToolStripMenuItem4.Size = New System.Drawing.Size(152, 22)
+        Me.ToolStripMenuItem4.Text = "+ 8%"
+        '
+        'ToolStripMenuItem5
+        '
+        Me.ToolStripMenuItem5.Name = "ToolStripMenuItem5"
+        Me.ToolStripMenuItem5.Size = New System.Drawing.Size(152, 22)
+        Me.ToolStripMenuItem5.Text = "+ 10%"
+        '
+        'AboutToolStripMenuItem
+        '
+        Me.AboutToolStripMenuItem.Name = "AboutToolStripMenuItem"
+        Me.AboutToolStripMenuItem.Size = New System.Drawing.Size(52, 20)
+        Me.AboutToolStripMenuItem.Text = "About"
+        '
+        'RichTextBox1
+        '
+        Me.RichTextBox1.BackColor = System.Drawing.SystemColors.ControlLightLight
+        Me.RichTextBox1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.RichTextBox1.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.RichTextBox1.Location = New System.Drawing.Point(0, 24)
+        Me.RichTextBox1.Name = "RichTextBox1"
+        Me.RichTextBox1.Size = New System.Drawing.Size(843, 403)
+        Me.RichTextBox1.TabIndex = 1
+        Me.RichTextBox1.Text = ""
+        '
+        'OpenFileDialog1
+        '
+        Me.OpenFileDialog1.FileName = "OpenFileDialog1"
+        '
+        'HelpToolStripMenuItem
+        '
+        Me.HelpToolStripMenuItem.Name = "HelpToolStripMenuItem"
+        Me.HelpToolStripMenuItem.Size = New System.Drawing.Size(44, 20)
+        Me.HelpToolStripMenuItem.Text = "Help"
+        '
+        'NotepadADV
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(843, 427)
+        Me.Controls.Add(Me.RichTextBox1)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.Name = "NotepadADV"
+        Me.Text = "NotepadADV"
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents MenuStrip1 As System.Windows.Forms.MenuStrip
+    Friend WithEvents FileToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents NewFileToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents OpenFileToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents SaveFileToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents ExitNotepadADVToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents EditToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents CutToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents CopyToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents PasteToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents UndoToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents RedoOperationToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents FormatToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents FontFormattingToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents ColorFormattingToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents AlignTextToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents LeftToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents RightToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents CenterToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents InsertBulletsToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents YesToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents NoToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents ZoomLevelToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents ToolStripMenuItem2 As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents ToolStripMenuItem3 As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents ToolStripMenuItem4 As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents ToolStripMenuItem5 As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents AboutToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents RichTextBox1 As System.Windows.Forms.RichTextBox
+    Friend WithEvents SaveFileDialog1 As System.Windows.Forms.SaveFileDialog
+    Friend WithEvents OpenFileDialog1 As System.Windows.Forms.OpenFileDialog
+    Friend WithEvents FontDialog1 As System.Windows.Forms.FontDialog
+    Friend WithEvents ColorDialog1 As System.Windows.Forms.ColorDialog
+    Friend WithEvents HelpToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+
+End Class
---
+++ notepadADV/Form1.vb
@@ -0,0 +1,100 @@
+﻿Public Class NotepadADV
+
+    Private Sub NewFileToolStripMenuItem_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles NewFileToolStripMenuItem.Click
+        RichTextBox1.Clear()
+    End Sub
+
+    Private Sub OpenFileToolStripMenuItem_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles OpenFileToolStripMenuItem.Click
+        OpenFileDialog1.ShowDialog()
+        RichTextBox1.LoadFile(OpenFileDialog1.FileName)
+    End Sub
+
+    Private Sub SaveFileToolStripMenuItem_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles SaveFileToolStripMenuItem.Click
+        SaveFileDialog1.ShowDialog()
+        RichTextBox1.SaveFile(SaveFileDialog1.FileName)
+        MsgBox("Your file has been saved.")
+
+    End Sub
+
+    Private Sub ExitNotepadADVToolStripMenuItem_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles ExitNotepadADVToolStripMenuItem.Click
+        End
+    End Sub
+
+    Private Sub CutToolStripMenuItem_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles CutToolStripMenuItem.Click
+        RichTextBox1.Cut()
+    End Sub
+
+    Private Sub CopyToolStripMenuItem_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles CopyToolStripMenuItem.Click
+        RichTextBox1.Copy()
+    End Sub
+
+    Private Sub PasteToolStripMenuItem_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles PasteToolStripMenuItem.Click
+        RichTextBox1.Paste()
+    End Sub
+
+    Private Sub UndoToolStripMenuItem_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles UndoToolStripMenuItem.Click
+        RichTextBox1.Undo()
+    End Sub
+
+    Private Sub RedoOperationToolStripMenuItem_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles RedoOperationToolStripMenuItem.Click
+        RichTextBox1.Redo()
+    End Sub
+
+    Private Sub FontFormattingToolStripMenuItem_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles FontFormattingToolStripMenuItem.Click
+        FontDialog1.ShowDialog()
+        RichTextBox1.SelectionFont = FontDialog1.Font
+    End Sub
+
+    Private Sub ColorFormattingToolStripMenuItem_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles ColorFormattingToolStripMenuItem.Click
+        ColorDialog1.ShowDialog()
+        RichTextBox1.SelectionColor = ColorDialog1.Color
+    End Sub
+
+    Private Sub LeftToolStripMenuItem_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles LeftToolStripMenuItem.Click
+        RichTextBox1.SelectionAlignment = HorizontalAlignment.Left
+    End Sub
+
+    Private Sub RightToolStripMenuItem_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles RightToolStripMenuItem.Click
+        RichTextBox1.SelectionAlignment = HorizontalAlignment.Right
+    End Sub
+
+    Private Sub CenterToolStripMenuItem_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles CenterToolStripMenuItem.Click
+        RichTextBox1.SelectionAlignment = HorizontalAlignment.Center
+    End Sub
+
+    Private Sub YesToolStripMenuItem_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles YesToolStripMenuItem.Click
+        RichTextBox1.SelectionBullet = True
+    End Sub
+
+    Private Sub NoToolStripMenuItem_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles NoToolStripMenuItem.Click
+        RichTextBox1.SelectionBullet = False
+    End Sub
+
+    Private Sub ToolStripMenuItem2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles ToolStripMenuItem2.Click
+        RichTextBox1.ZoomFactor = 2
+    End Sub
+
+    Private Sub ToolStripMenuItem3_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles ToolStripMenuItem3.Click
+        RichTextBox1.ZoomFactor = 4
+    End Sub
+
+    Private Sub ToolStripMenuItem4_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles ToolStripMenuItem4.Click
+        RichTextBox1.ZoomFactor = 8
+    End Sub
+
+    Private Sub ToolStripMenuItem5_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles ToolStripMenuItem5.Click
+        RichTextBox1.ZoomFactor = 10
+    End Sub
+
+    Private Sub AboutToolStripMenuItem_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles AboutToolStripMenuItem.Click
+        MessageBox.Show("NotepadADV is a simple rich text formatter developed by Bedanta Dey. Inspired by Microsoft Notepad.", "About NotepadADV", MessageBoxButtons.OK)
+    End Sub
+
+    Private Sub FormatToolStripMenuItem_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles FormatToolStripMenuItem.Click
+
+    End Sub
+
+    Private Sub HelpToolStripMenuItem_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles HelpToolStripMenuItem.Click
+        MessageBox.Show("Why can't I open my saved files? - Because they are saved in no format. While saving, please append the file name with `.rtf`. | Why is there no help section? - I am working on it!", "Frequently Asked Questions", MessageBoxButtons.OK)
+    End Sub
+End Class
